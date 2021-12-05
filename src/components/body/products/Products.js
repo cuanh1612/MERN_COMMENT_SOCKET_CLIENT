d5923c743664d3f229621443e495f1a0ed94a665
Nguyen Quang Huy <huynguyencc1000@gmail.com>
2021-12-05 21:21:20 +0700

    add loading products

--- src/components/body/products/Products.js
+++ src/components/body/products/Products.js
@@ -1,4 +1,4 @@
-import { Container, SimpleGrid } from '@chakra-ui/react'
+import { Container, SimpleGrid, Spinner, Center } from '@chakra-ui/react'
 import React, { useContext } from 'react'
 import { DataContext } from '../../../GlobalState'
 import ProductCard from '../../utils/productCard/ProductCard'
@@ -8,15 +8,30 @@ const Products = () => {
     const [Products] = state.products
     return (
         <>
-            <Container maxW='container.xl'>
-                <SimpleGrid columns={[1, 2, 2, 3]} spacing={10}>
-                    {
-                        Products.map(product => (
-                            <ProductCard key={product._id} product={product} />
-                        ))
-                    }
-                </SimpleGrid>
-            </Container>
+            {
+                Products && Products.length > 0 ? (
+                    <Container maxW='container.xl'>
+                        <SimpleGrid columns={[1, 2, 2, 3]} spacing={10}>
+                            {
+                                Products.map(product => (
+                                    <ProductCard key={product._id} product={product} />
+                                ))
+                            }
+                        </SimpleGrid>
+                    </Container>
+                )
+                    : (
+                        <Center>
+                            <Spinner
+                                thickness='4px'
+                                speed='0.65s'
+                                emptyColor='gray.200'
+                                color='blue.500'
+                                size='xl'
+                            />
+                        </Center>
+                    )
+            }
         </>
     )
 }
